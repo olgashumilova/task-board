9b91664b7964877e2e0bb981cc53f6f32367828d
Volha Shumilava <volha@prezzee.com>
2023-02-21 21:32:46 +0300

    server

---
+++ src/api/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// Get queries
+
+// Post queries
+export const getHelpEmail = "http://localhost:3001/getHelpEmail";
+
+// Common query
--- src/pages/LandingPage/LandindPage.tsx
+++ src/pages/LandingPage/LandindPage.tsx
@@ -1,6 +1,9 @@
-import { Box, Button, Input, Stack, Typography } from "@mui/material/";
+import React, { useState } from "react";
+import axios from "axios";
+import { Box, Button, Input, Stack, Typography, Modal } from "@mui/material/";
 import landingImage from "../../assets/landing-image.svg";
 import { useTranslation } from "react-i18next";
+import { getHelpEmail } from "../../api/api";
 
 const styles = {
   titleContainer: {
@@ -8,10 +11,42 @@ const styles = {
     boxShadow: "0px 5px 34px 43px rgba(146, 227, 169, 0.6)",
     width: "90%",
   },
+  modal: {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    transform: "translate(-50%, -50%)",
+    width: "40%",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 2,
+  },
 };
 
 const LandingPage = () => {
   const { t } = useTranslation();
+
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [modalResponse, setModalResponse] = useState("");
+
+  const handleOpen = async () => {
+    if (email !== "") {
+      const response = await axios.post(getHelpEmail, { email });
+      setModalResponse(response.data);
+      setOpen(response.data !== "");
+      console.log(response);
+      console.log(email);
+    }
+  };
+  const handleClose = () => {
+    setEmail("");
+    setOpen(false);
+  };
 
   return (
     <Stack direction="row" padding={8}>
@@ -27,14 +62,24 @@ const LandingPage = () => {
           <Typography variant="h5" color={"#00433B"}>
             {t("landingPage.formTitle")}
           </Typography>
-          <Input placeholder="example@mail.com" />
-          <Button variant="contained" color="success">
+          <Input
+            placeholder="example@mail.com"
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+          />
+          <Button variant="contained" color="success" onClick={handleOpen}>
             {t("landingPage.formButton")}
           </Button>
+
+          <Modal open={open} onClose={handleClose}>
+            <Box sx={styles.modal}>
+              <Typography>{modalResponse}</Typography>
+            </Box>
+          </Modal>
         </Stack>
       </Stack>
 
-      <img width={"50%"} src={landingImage} alt="Landing image" />
+      <img width={"50%"} src={landingImage} alt="Task board" />
     </Stack>
   );
 };
