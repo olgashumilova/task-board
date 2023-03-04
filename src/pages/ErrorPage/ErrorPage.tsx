838bf8d7be1a43920f0a565bec7600400ad2d58e
Volha Shumilava <volha@prezzee.com>
2023-03-04 21:45:08 +0300

    styles for error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,14 +1,20 @@
-import { Stack, TextField, Button, Typography, Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Stack, Button, Typography, Box } from "@mui/material";
 import { ROUTES } from "../../routes";
+import errorImage from "../../assets/error-image.svg";
 
 const ErrorPage = () => {
   return (
-    <Stack>
-      <Typography>Такой страницы не существует</Typography>
-      {/* <Link to={ROUTES.HOME}> */}
-      <Button>На главную</Button>
-      {/* </Link> */}
+    <Stack alignItems={"center"} height={"100vh"} justifyContent={"center"}>
+      <Typography fontSize={"3rem"}>Упс!</Typography>
+      <img width={"50%"} src={errorImage} alt="Task board" />
+      <Typography fontSize={"2rem"} mt={4} mb={4} textAlign={"center"}>
+        Такой страницы не существует
+      </Typography>
+      <Box mb={4}>
+        <Button variant="contained" color="success" href={ROUTES.HOME}>
+          На главную
+        </Button>
+      </Box>
     </Stack>
   );
 };
